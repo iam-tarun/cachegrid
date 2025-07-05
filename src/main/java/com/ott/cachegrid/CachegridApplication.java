a854dc38729a80f6f03797d4fac5c3cf2bbe365d
Tarunteja Obbina <tarun@Taruntejas-MacBook-Pro.local>
2025-07-05 13:21:56 -0500

    default setup

---
+++ src/main/java/com/ott/cachegrid/CachegridApplication.java
@@ -0,0 +1,13 @@
+package com.ott.cachegrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachegridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachegridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ott/cachegrid/CachegridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ott.cachegrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachegridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
